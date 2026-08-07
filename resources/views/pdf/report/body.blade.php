edb4c8c082bee8cef482a7044bb4ae33fa0d5cd3
Ricardo Valladares <lerichard@Ricardos-MacBook-Pro.local>
2026-08-07 13:00:48 -0600

    Agregando cambio de quitar tipo

--- resources/views/pdf/report/body.blade.php
+++ resources/views/pdf/report/body.blade.php
@@ -514,13 +514,13 @@
                                     <strong>Nombre:</strong> {{ $customer->name ?? 'N/A' }}<br>
                                     <strong>Edad:</strong> {{ $customer->age ?? 'N/A' }} años &nbsp;&nbsp;&nbsp; <strong>Sexo:</strong> {{ $customer->gender === 'M' || $customer->gender === 'masculino' || $customer->gender === 'Masculino' ? 'M' : 'F' }}<br>
                                     <strong>Médico Remitente:</strong> {{ $referrer->name ?? 'N/A' }}<br>
-                                    <strong>Hospital/Clínica:</strong> {{ $referrer->notes ?? '' }}<br>
+                                    <strong>Hospital/Clínica:</strong> {{ $referrer->notes ?? '' }}
                                 </td>
                                 <td style="width: 45%; padding-left: 12px;">
-                                    <strong>Tipo de muestra:</strong> {{ ($specimen->type->name ?? 'N/A') . ' - ' . ($specimen->examination->name ?? 'N/A') }}<br>
                                     <strong>Diagnóstico Clínicos:</strong> {{ $specimen->diagnosis ?? '' }}<br>
                                     <strong>Sitio Anatómico:</strong> {{ $specimen->anatomic_site ?? 'N/A' }}<br>
-                                    <strong>Fecha de Recepción:</strong> {{ $report->report_date ? \Carbon\Carbon::parse($report->report_date)->format('d/m/Y') : 'N/A' }} &nbsp;&nbsp;&nbsp; <strong>Fecha de la toma:</strong> {{ $report->sample_collection_date ? \Carbon\Carbon::parse($report->sample_collection_date)->format('d/m/Y') : 'N/A' }}
+                                    <strong>Fecha de Recepción:</strong> {{ $report->report_date ? \Carbon\Carbon::parse($report->report_date)->format('d/m/Y') : 'N/A' }}<br>
+									<strong>Fecha de la toma:</strong> {{ $report->sample_collection_date ? \Carbon\Carbon::parse($report->sample_collection_date)->format('d/m/Y') : 'N/A' }}
                                 </td>
                             </tr>
                         </table>
